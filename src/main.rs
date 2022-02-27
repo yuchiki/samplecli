4b7da545f8ce1ef50c1361e2479bc3fec771737d
yuchiki <yuki082821993@gmail.com>
2022-02-27 23:45:45 +0900

    p151made

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
-use std::env;
+use clap::{App, Arg};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    let matches = App::new("My RPN program")
+        .version("1.0.0")
+        .author("yuchiki")
+        .about("Super awesome sample RPN calculator")
+        .arg(
+            Arg::new("formula file")
+                .value_name("FILE")
+                .index(1)
+                .required(false),
+        )
+        .arg(
+            Arg::new("verbose")
+                .short('v')
+                .long("verbose")
+                .required(false),
+        )
+        .get_matches();
+
+    match matches.value_of("formula file") {
+        Some(file) => println!("File specified: {}", file),
+        None => println!("No file speficied"),
+    }
+    let verbose = matches.is_present("verbose");
+    println!("Is verbosity specified?: {}", verbose);
 }
